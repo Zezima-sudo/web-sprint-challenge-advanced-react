fdb0f1181ce3be904c2473c10ade81d6c5902626
Bruno Eire <ericmichaelrice@protonmail.com>
2020-09-04 12:30:29 -0400

    Mapped data to screen + custom hook WIP

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,10 @@
 // write your custom hook here to control your checkout form
+import {React, useState} from 'react'
+
+const useForm = () => {
+    const [form, setForm] = useState(key, initialValue)
+
+    const handleChanges = () => {
+        
+    }
+}
